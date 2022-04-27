aeedfab7e7f1288e46f8d03213ad20a223ae0142
Evgeniy Saltykov <saltykov.evgen@mail.ru>
2022-04-27 18:01:36 +0300

    add trash

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 let btnEdit = document.querySelector('.profile__edit');
 let editForm = document.querySelector('.popup');
 let btnCloseForm = editForm.querySelector('.popup__close');
+let formTitle = editForm.querySelector('.form__title');
 let formInputName = editForm.querySelector('.form__input_text_name');
 let formInputWork = editForm.querySelector('.form__input_text_work');
 let profileName = document.querySelector('.profile__name');
 let profileWork = document.querySelector('.profile__description');
+let btnAddImg = document.querySelector('.profile__add');
 
 function fillInput() {
   formInputName.value = profileName.textContent;
@@ -12,7 +14,17 @@ function fillInput() {
 }
 
 function openEditForm() {
+  console.log('btnN');
+  formTitle.textContent = `Редактировать профиль`;
   fillInput();
+  editForm.classList.add('popup_opened');
+}
+
+function openAddImgForm() {
+  console.log('2');
+  formTitle.textContent = `Новое место`;
+  formInputName.value = `Название`;
+  formInputWork.value = `Ссылка на картинку`;
   editForm.classList.add('popup_opened');
 }
 
@@ -36,7 +48,7 @@ function formSubmitHandler(event) {
   closedEditForm();
 }
 
-
 btnEdit.addEventListener('click', openEditForm);
+btnAddImg.addEventListener('click', openAddImgForm);
 btnCloseForm.addEventListener('click', closedEditForm);
 editForm.addEventListener('submit', formSubmitHandler);
