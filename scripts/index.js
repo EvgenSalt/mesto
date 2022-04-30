4dda907fbcb7a518c48da223012f7d52c1d93216
Evgeniy Saltykov <saltykov.evgen@mail.ru>
2022-04-30 13:08:09 +0300

    add temlate & add img popap

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,57 @@
-let btnEdit = document.querySelector('.profile__edit');
-let editForm = document.querySelector('.popup');
-let btnCloseForm = editForm.querySelector('.popup__close');
-let formTitle = editForm.querySelector('.form__title');
-let formInputName = editForm.querySelector('.form__input_text_name');
-let formInputWork = editForm.querySelector('.form__input_text_work');
-let profileName = document.querySelector('.profile__name');
-let profileWork = document.querySelector('.profile__description');
-let btnAddImg = document.querySelector('.profile__add');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const btnEdit = document.querySelector('.profile__edit');
+const editForm = document.querySelector('.popup__edit-form');
+const addImgForm = document.querySelector('.popup__add-img');
+const btnCloseEditForm = editForm.querySelector('.close__edit-form');
+const btnCloseAddImgForm = addImgForm.querySelector('.close__add-img');
+const formTitle = editForm.querySelector('.form__title');
+const formInputName = editForm.querySelector('.form__input_text_name');
+const formInputWork = editForm.querySelector('.form__input_text_work');
+const profileName = document.querySelector('.profile__name');
+const profileWork = document.querySelector('.profile__description');
+const btnAddImg = document.querySelector('.profile__add');
+const listElements = document.querySelector('.elements__items');
+const templateCard = document.querySelector('.template__card');
+
+function renderCard() {
+  const cardHTML = initialCards.map(getElementCard);
+  listElements.append(...cardHTML);
+}
+
+function getElementCard(item) {
+  const getElementTemplateCard = templateCard.content.cloneNode(true);
+  const nameCard = getElementTemplateCard.querySelector('.elements__text');
+  const imgCard = getElementTemplateCard.querySelector('.elements__img');
+  imgCard.src = item.link;
+  nameCard.textContent = item.name;
+  return getElementTemplateCard;
+}
 
 function fillInput() {
   formInputName.value = profileName.textContent;
@@ -14,23 +59,28 @@ function fillInput() {
 }
 
 function openEditForm() {
-  console.log('btnN');
-  formTitle.textContent = `Редактировать профиль`;
   fillInput();
   editForm.classList.add('popup_opened');
 }
 
 function openAddImgForm() {
-  console.log('2');
-  formTitle.textContent = `Новое место`;
-  formInputName.value = `Название`;
-  formInputWork.value = `Ссылка на картинку`;
-  editForm.classList.add('popup_opened');
+  addImgForm.classList.add('popup_opened');
 }
 
 function closedEditForm() {
   fillInput();
   editForm.classList.remove('popup_opened');
+}
+
+function closedAddImgForm() {
+  addImgForm.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+  profileName.textContent = formInputName.value;
+  profileWork.textContent = formInputWork.value;
+  closedEditForm();
 }
 
 /*  функция сохранения формы при нажатии на оверлэй  */
@@ -41,14 +91,10 @@ function closedEditForm() {
 // }
 // editForm.addEventListener('click', onOverlayClick);
 
-function formSubmitHandler(event) {
-  event.preventDefault();
-  profileName.textContent = formInputName.value;
-  profileWork.textContent = formInputWork.value;
-  closedEditForm();
-}
-
 btnEdit.addEventListener('click', openEditForm);
-btnAddImg.addEventListener('click', openAddImgForm);
-btnCloseForm.addEventListener('click', closedEditForm);
+btnAddImg.addEventListener('click', openAddImgForm);//
+btnCloseEditForm.addEventListener('click', closedEditForm);
+btnCloseAddImgForm.addEventListener('click', closedAddImgForm);
 editForm.addEventListener('submit', formSubmitHandler);
+
+renderCard();
